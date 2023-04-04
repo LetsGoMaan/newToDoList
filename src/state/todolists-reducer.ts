31baad79b80b033b8a62d36da1dfa3cccf0ebb28
Andrey Prokhorov <ilikeseik@gmail.com>
2023-04-05 01:22:45 +0300

    8. Reducers, unit tests, actions, action creators

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,67 @@
+import {FilterButtonType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+
+
+export type RemoveTodolistAT = {
+    type: "REMOVE-TODOLIST"
+    id: string
+}
+export type AddTodolistAT = {
+    type: "ADD-TODOLIST"
+    title: string
+}
+export type ChangeTodolistTitleAT = {
+    type: "CHANGE-TODOLIST-TITLE",
+    id: string
+    title: string
+}
+export type ChangeTodolistFilterAT = {
+    type: "CHANGE-TODOLIST-FILTER",
+    id: string
+    filter: FilterButtonType
+}
+
+type ActionsType = RemoveTodolistAT | AddTodolistAT |  ChangeTodolistTitleAT | ChangeTodolistFilterAT
+
+// меня вызовут и дадут мне стейт (почти всегда объект)
+// и инструкцию (action, тоже объект)
+// согласно прописанному type в этом action (инструкции) я поменяю state
+export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType):Array<TodolistType> => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST':
+            return state.filter(todo => todo.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [...state, {id: v1(), title: action.title, filter: "all"}]
+        case "CHANGE-TODOLIST-TITLE":
+        let todolist = state.find(todo => todo.id === action.id)
+            if(todolist) {
+                todolist.title = action.title
+            }
+            return [...state]
+        case "CHANGE-TODOLIST-FILTER": {
+            let todolist = state.find(todo => todo.id === action.id)
+            if(todolist) {
+                todolist.filter = action.filter
+            }
+            return [...state]
+        }
+
+        default:
+            throw new Error('I don\'t understand this type')
+    }
+}
+
+export const RemoveTodolistAC = (todolistId: string): RemoveTodolistAT => {
+    return {type: 'REMOVE-TODOLIST', id: todolistId}
+}
+export const AddTodolistAC = (title: string): AddTodolistAT => {
+    return {type: 'ADD-TODOLIST', title: title}
+}
+export const ChangeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleAT => {
+    return {type: 'CHANGE-TODOLIST-TITLE', id: id ,title: title}
+}
+export const ChangeTodolistFilterAC = (id: string, filter: FilterButtonType): ChangeTodolistFilterAT => {
+    return {type: 'CHANGE-TODOLIST-FILTER', id: id ,filter: filter}
+}
+
